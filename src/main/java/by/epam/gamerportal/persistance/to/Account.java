e01b3caad86ff0d0ffb1ff823a3fc65cacde43c7
Yayheniy Lepkovich <yayheniy_lepkovich@epam.com>
2016-10-28 18:39:37 +0300

    first commit

---
+++ src/main/java/by/epam/gamerportal/persistance/to/Account.java
@@ -0,0 +1,10 @@
+package by.epam.gamerportal.persistance.to;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Yayheniy_Lepkovich on 10/28/2016.
+ */
+
+public class Account {
+}
