ea2e376e834b8793af9dd3e3ffe781220d1fd3a1
Yayheniy Lepkovich <yayheniy_lepkovich@epam.com>
2016-11-08 10:14:09 +0300

    init boot config

--- src/main/java/by/epam/gamerportal/config/AppConfiguration.java
+++ src/main/java/by/epam/gamerportal/config/AppConfiguration.java
@@ -12,23 +12,23 @@ import org.springframework.web.servlet.view.JstlView;
 /**
  * Created by Yayheniy_Lepkovich on 11/3/2016.
  */
-//@SpringBootApplication
+@SpringBootApplication
 public class AppConfiguration extends SpringBootServletInitializer {
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(AppConfiguration.class);
-//    }
-//
-//    @Bean
-//    public ViewResolver getViewResolver(){
-//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-//        resolver.setPrefix("/WEB-INF/jsp/");
-//        resolver.setSuffix(".jsp");
-//        resolver.setViewClass(JstlView.class);
-//        return resolver;
-//    }
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(AppConfiguration.class, args);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppConfiguration.class);
+    }
+
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfiguration.class, args);
+    }
 }
